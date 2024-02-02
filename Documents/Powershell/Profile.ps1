1bafb7855617084967708960256d1acb095e3e3b
Kohey <very.tasty.konjac@gmail.com>
2024-02-02 19:26:47 +0900

    windows

---
+++ Documents/Powershell/Profile.ps1
@@ -0,0 +1,17 @@
+# cd
+
+function SetLocationUp { Set-Location .. }
+New-Alias .. SetLocationUp
+
+# git
+
+New-Alias g git
+
+# nvim
+
+New-Alias vi nvim
+New-Alias vim nvim
+
+# docker
+
+New-Alias d docker
