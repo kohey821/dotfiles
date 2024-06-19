7ef3fea61349a9430c0017cb96a14da9728944ed
Kohey <very.tasty.konjac@gmail.com>
2024-06-19 13:33:57 +0900

    ps

--- Documents/Powershell/Profile.ps1
+++ Documents/Powershell/Profile.ps1
@@ -15,3 +15,20 @@ New-Alias vim nvim
 # docker
 
 New-Alias d docker
+
+# update & cleanup
+
+function PackageUpdate {
+    winget upgrade -r --accept-package-agreements --accept-source-agreements
+}
+New-Alias package-update PackageUpdate
+
+function DockerCleanup {
+    docker system prune
+}
+New-Alias docker-cleanup DockerCleanup
+
+function UC {
+    PackageUpdate && DockerCleanup
+}
+New-Alias uc UC
