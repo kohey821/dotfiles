6cfceab5dcf74dd16a66f6eea333a4744b3a46c8
Kohey <very.tasty.konjac@gmail.com>
2024-08-08 23:26:56 +0900

    vim plug

--- set-up.ps1
+++ set-up.ps1
@@ -2,7 +2,7 @@
 
 winget install JanDeDobbeleer.OhMyPosh -s winget
 
-# nvim
+# neovim
 
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
