ac3a4e57cd58ff2a1b5928e366ed1a7dfe861ecb
Kohey <very.tasty.konjac@gmail.com>
2024-10-10 00:20:45 +0900

    ps1

--- dot_my-config/executable_init.ps1
+++ dot_my-config/executable_init.ps1
@@ -4,7 +4,13 @@ winget install JanDeDobbeleer.OhMyPosh -s winget
 
 # neovim
 
-iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
+Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    New-Item "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 
-nvim -c 'PlugUpdate' -c 'qa'
+#
+
+Write-Output "
+include scripts:
+  ps:
+    . ~\.my-config\ps\init.ps1
+"
