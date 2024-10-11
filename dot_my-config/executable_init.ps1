625e008812b4803066df88ddb99c05e611733d38
Kohey <very.tasty.konjac@gmail.com>
2024-10-12 02:00:59 +0900

    comment

--- dot_my-config/executable_init.ps1
+++ dot_my-config/executable_init.ps1
@@ -2,10 +2,10 @@
 
 winget install JanDeDobbeleer.OhMyPosh -s winget
 
-# neovim
+# vim-plug
 
 Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-    New-Item "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
+  New-Item "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 
 #
 
