77ec71cb9f16dc57abdd375c9cfa667fcc59162f
Kohey <very.tasty.konjac@gmail.com>
2024-10-19 17:59:23 +0900

    直接書く

--- dot_my-config/executable_init.ps1
+++ dot_my-config/executable_init.ps1
@@ -6,11 +6,3 @@ winget install JanDeDobbeleer.OhMyPosh -s winget
 
 Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
   New-Item "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
-
-#
-
-Write-Output "
-include scripts:
-  ps:
-    . ~\.my-config\ps\init.ps1
-"
---
+++ dot_vimrc
@@ -0,0 +1,1 @@
+source ~/.my-config/vim/init.vim
