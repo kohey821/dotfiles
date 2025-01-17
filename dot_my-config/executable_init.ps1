1eb7b4b9f51e91ead135f3baa54cc48d2e058cb0
Kohey <very.tasty.konjac@gmail.com>
2025-01-18 02:37:59 +0900

    ps

--- dot_my-config/executable_init.ps1
+++ dot_my-config/executable_init.ps1
@@ -1,18 +1,5 @@
 # install packages
 
-winget install sharkdp.fd
-winget install fzf
 winget install --id Git.Git -e --source winget
-winget install dandavison.delta
-winget install jqlang.jq
-winget install Neovim.Neovim
 winget install --id Microsoft.PowerShell --source winget
-winget install BurntSushi.ripgrep.MSVC
-winget install TortoiseSVN.TortoiseSVN
-
-winget install JanDeDobbeleer.OhMyPosh -s winget
-
-# vim-plug
-
-Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-  New-Item "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
+winget install dandavison.delta
