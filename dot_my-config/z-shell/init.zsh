658168f58f569d145cb95bf1023060e6a0d6d8fc
Kohey <very.tasty.konjac@gmail.com>
2025-02-22 13:46:07 +0900

    asdf path

--- dot_my-config/z-shell/init.zsh
+++ dot_my-config/z-shell/init.zsh
@@ -2,6 +2,8 @@
 
 # asdf
 
+export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+
 # append completions to fpath
 fpath=(${ASDF_DATA_DIR:-$HOME/.asdf}/completions $fpath)
 # initialise completions with ZSH's compinit
