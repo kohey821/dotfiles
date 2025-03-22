6c6226ea9798d5952aadd5d99eb61e2670749d94
Kohey <very.tasty.konjac@gmail.com>
2025-03-22 23:31:54 +0900

    asdf

--- dot_my-config/init/darwin.sh
+++ dot_my-config/init/darwin.sh
@@ -1,11 +1,6 @@
-# brew
-
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-#
-
 brew install \
-    asdf \
     coreutils \
     curl \
     fish \
@@ -18,4 +13,14 @@ brew install \
     tmux \
     tmuxp \
     unzip \
+    wget \
     zsh
+
+# asdf. NOTE: 本当はbrewでInstallしたいが、Completionがうまく設定されないような気がするので
+
+cd /tmp
+wget https://github.com/asdf-vm/asdf/releases/download/v0.16.6/asdf-v0.16.6-darwin-arm64.tar.gz
+tar -xzv -f asdf-v0.16.6-darwin-arm64.tar.gz
+mv asdf $HOME/bin/
+rm asdf-v0.16.6-darwin-arm64.tar.gz
+cd -
--- dot_my-config/init/linux.sh
+++ dot_my-config/init/linux.sh
@@ -1,28 +1,3 @@
-# asdf
-
-filename='asdf-v0.16.4-linux-amd64.tar.gz'
-cd /tmp
-wget https://github.com/asdf-vm/asdf/releases/download/v0.16.4/${filename}
-tar -xzv -f ${filename}
-test ! -d $HOME/bin && mkdir $HOME/bin
-mv asdf $HOME/bin/
-rm ${filename}
-cd -
-
-# delta
-
-filename='git-delta_0.18.2_amd64.deb'
-cd /tmp
-wget https://github.com/dandavison/delta/releases/download/0.18.2/${filename}
-sudo apt install -y ./${filename}
-rm ${filename}
-cd -
-
-# NOTE: すぐ使うので
-PATH=$PATH:$HOME/bin
-
-#
-
 sudo apt update && sudo apt upgrade -y && sudo apt install -y \
     build-essential \
     curl \
@@ -35,5 +10,23 @@ sudo apt update && sudo apt upgrade -y && sudo apt install -y \
     tmux \
     tmuxp \
     unzip \
+    wget \
     xclip \
     zsh
+
+# asdf
+
+cd /tmp
+wget https://github.com/asdf-vm/asdf/releases/download/v0.16.6/asdf-v0.16.6-linux-amd64.tar.gz
+tar -xzv -f asdf-v0.16.6-linux-amd64.tar.gz
+mv asdf $HOME/bin/
+rm asdf-v0.16.6-linux-amd64.tar.gz
+cd -
+
+# delta
+
+cd /tmp
+wget https://github.com/dandavison/delta/releases/download/0.18.2/git-delta_0.18.2_amd64.deb
+sudo apt install -y ./git-delta_0.18.2_amd64.deb
+rm git-delta_0.18.2_amd64.deb
+cd -
