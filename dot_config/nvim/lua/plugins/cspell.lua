dab72555fe2932b1b4a95c28ccfe22ce6d78a49c
Kohey <very.tasty.konjac@gmail.com>
2025-07-14 02:34:53 +0900

    cspell

---
+++ dot_config/nvim/lua/plugins/cspell.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "cspell",
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      -- Event to trigger linters
+      events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+      linters_by_ft = {
+        ["*"] = { "cspell" },
+      },
+    },
+  },
+}
