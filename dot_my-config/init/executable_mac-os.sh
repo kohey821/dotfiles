1bfab93f4aae6fb72ee449bc750f20c74b9894c4
kohey821 <very.tasty.konjac@gmail.com>
2025-12-12 15:32:29 +0900

    macOS init

--- dot_my-config/init/executable_mac-os.sh
+++ dot_my-config/init/executable_mac-os.sh
@@ -1,7 +1,5 @@
 #
 
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 brew install \
   coreutils \
   curl \
@@ -24,13 +22,3 @@ brew install \
 #
 
 . $HOME/.my-config/init/common.sh
-
-# tasks
-
-cat <<'EOS'
-
-tasks:
-- add 'eval "$(brew shellenv)' to bash's config
-- add 'eval "$(brew shellenv)' to z-shell's config
-
-EOS
