66301d8ce3283d2dff9f5c32ed36e2a326e4280a
Kohey <very.tasty.konjac@gmail.com>
2026-01-19 05:29:27 +0900

    Init Z Shell

--- dot_config/dotfiles/init/executable_arch-linux.sh
+++ dot_config/dotfiles/init/executable_arch-linux.sh
@@ -6,6 +6,8 @@ sudo pacman -Syu --noconfirm --needed
 
 sudo pacman -S --noconfirm --needed yay
 
+sudo pacman -S --noconfirm --needed zsh
+
 sudo pacman -S --noconfirm --needed \
   curl \
   fish \
