997ea94569b06d75d17e0a1c80d46134c0f1568f
Kohey <very.tasty.konjac@gmail.com>
2026-03-17 14:22:33 +0900

    Completion 用 Keymap はいいや

--- dot_config/nvim/lua/plugins/cmp.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "hrsh7th/nvim-cmp",
-  opts = function(_, opts)
-    local cmp = require("cmp")
-    opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<A-esc>"] = cmp.mapping.complete(),
-    })
-  end,
-}
