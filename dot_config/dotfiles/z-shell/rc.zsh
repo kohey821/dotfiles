744c26ab0781bbd254ba720bc7bb99aed14b0740
Kohey <very.tasty.konjac@gmail.com>
2026-04-01 17:21:52 +0900

    PATH で判断するっぺ
    055867c625ec280c5449a0f8164f4feb31fcce16 はちょっと力技すぎたっぺ。

--- dot_config/dotfiles/z-shell/rc.zsh
+++ dot_config/dotfiles/z-shell/rc.zsh
@@ -17,10 +17,9 @@ compinit
 
 # Setup
 
-eval "$(mise activate zsh)"
-# NOTE: macOS (brew) では `eval "$(mise activate zsh)"` を実行すると
-# 補完が壊れる？っぽい (brew と競合してる？) ので手動で source する。
-source <(mise completion zsh)
+# NOTE: macOS brew ではすでに実行されていて？もう一度実行すると Completion が壊れる？ので
+# PATH に Mise で Install したもの (`usage` は絶対 Install する) が無い場合に実行する
+echo $PATH | grep -q "/mise/installs/" || eval "$(mise activate zsh)"
 
 eval "$(starship init zsh)"
 eval "$(zoxide init zsh)"
