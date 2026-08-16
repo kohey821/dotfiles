b9f6be6a3e3470a9723274b35cc49ba03661a9c1
Kohey <very.tasty.konjac@gmail.com>
2026-08-16 18:28:40 +0900

    Aliases を外部 File に

--- dot_local/share/dotfiles/shell/rc.sh
+++ dot_local/share/dotfiles/shell/rc.sh
@@ -1,32 +1,7 @@
 export EDITOR=nvim
 
 
-alias d=docker
-
-alias f='find'
-
-alias g=git
-
-alias gr='grep'
-
-alias ls=eza
-alias la='ls -A'
-alias ll='ls -alF'
-
-alias rm='rm -i'
-
-alias rp='realpath'
-
-alias jj='jiten -v jmdict'
-alias jk='jiten -v kanji'
-alias js='jiten -v sentences'
-alias te='trans -t en'
-alias tj='trans -t ja'
-
-alias ta='tmux attach-session'
-alias tk='tmux kill-session'
-alias tl='tmux list-sessions'
-alias tn='tmux new-session'
+source "$(dirname "$(realpath "$0")")/aliases.sh"
 
 
 stty -ixon
